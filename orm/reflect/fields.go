8105cd95f6a221fc729e3687708deba5c1bcfc5e
v_frgfeng <v_frgfeng@tencent.com>
2023-07-04 15:43:38 +0800

    orm 反射修改字段

--- orm/reflect/fields.go
+++ orm/reflect/fields.go
@@ -1,6 +1,61 @@
 package reflect
 
+import (
+	"errors"
+	"reflect"
+)
+
 // IterateFields 遍历字段
 func IterateFields(entity any) (map[string]any, error) {
+	if entity == nil {
+		return nil, errors.New("不支持 nil")
+	}
+	typ := reflect.TypeOf(entity)
+	val := reflect.ValueOf(entity)
+	if val.IsZero() {
+		return nil, errors.New("不支持零值")
+	}
 
+	// 使用 for 的原因是可能是多重指针
+	for typ.Kind() == reflect.Pointer {
+		typ = typ.Elem()
+		val = val.Elem()
+	}
+	if typ.Kind() != reflect.Struct {
+		return nil, errors.New("不支持类型")
+	}
+
+	fieldNums := typ.NumField()
+
+	res := make(map[string]any, fieldNums)
+	for i := 0; i < fieldNums; i++ {
+		typName := typ.Field(i)
+		if typName.IsExported() {
+			res[typ.Field(i).Name] = val.Field(i).Interface()
+		} else {
+			res[typName.Name] = reflect.Zero(typName.Type).Interface()
+		}
+	}
+	return res, nil
+}
+
+func SetField(entity any, field string, newValue any) (err error) {
+	if entity == nil {
+		return errors.New("不支持 nil")
+	}
+	val := reflect.ValueOf(entity)
+	for val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	if val.Kind() != reflect.Struct {
+		return errors.New("不支持的类型")
+	}
+	fieldVal := val.FieldByName(field)
+	// 是否可以被修改
+	if !fieldVal.CanSet() {
+		return errors.New("不可修改字段")
+	}
+	fieldVal.Set(reflect.ValueOf(newValue))
+
+	return nil
 }
--- orm/reflect/fields_test.go
+++ orm/reflect/fields_test.go
@@ -1,6 +1,7 @@
 package reflect
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -28,8 +29,48 @@ func TestIterateFields(t *testing.T) {
 			},
 			wantRes: map[string]any{
 				"Name": "Tom",
-				"age":  18,
+				"age":  0,
 			},
+		},
+		{
+			name: "pointer struct",
+			entity: &User{
+				Name: "Tom",
+				age:  18,
+			},
+			wantRes: map[string]any{
+				"Name": "Tom",
+				"age":  0,
+			},
+		},
+		{
+			name: "multiple pointer struct",
+			entity: func() **User {
+				res := &User{
+					Name: "Tom",
+					age:  18,
+				}
+				return &res
+			}(),
+			wantRes: map[string]any{
+				"Name": "Tom",
+				"age":  0,
+			},
+		},
+		{
+			name:    "basic",
+			entity:  18,
+			wantErr: errors.New("不支持类型"),
+		},
+		{
+			name:    "nil",
+			entity:  nil,
+			wantErr: errors.New("不支持 nil"),
+		},
+		{
+			name:    "nil user",
+			entity:  (*User)(nil),
+			wantErr: errors.New("不支持零值"),
 		},
 	}
 
@@ -41,6 +82,59 @@ func TestIterateFields(t *testing.T) {
 				return
 			}
 			assert.Equal(t, tc.wantRes, res)
+		})
+	}
+
+}
+
+func TestSetField(t *testing.T) {
+	type User struct {
+		Name string
+		age  int
+	}
+	testCases := []struct {
+		name     string
+		entity   any
+		field    string
+		newValue any
+
+		wantErr    error
+		wantEntity any
+	}{
+		{
+			name:     "struct",
+			field:    "Name",
+			newValue: "Tim",
+			entity: User{
+				Name: "Tom",
+			},
+			wantEntity: User{
+				Name: "Tom",
+			},
+			wantErr: errors.New("不可修改字段"),
+		},
+		{
+			name:     "point",
+			field:    "Name",
+			newValue: "Tim",
+			entity: &User{
+				Name: "Tom",
+			},
+			wantEntity: &User{
+				Name: "Tim",
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := SetField(tc.entity, tc.field, tc.newValue)
+			assert.Equal(t, tc.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.entity, tc.wantEntity)
 		})
 	}
 
