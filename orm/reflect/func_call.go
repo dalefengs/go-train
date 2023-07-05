8d6cfe82869002d6e5bea299aea0f8b66b3aca66
v_frgfeng <v_frgfeng@tencent.com>
2023-07-05 15:52:24 +0800

    orm 反射方法

--- orm/reflect/func_call.go
+++ orm/reflect/func_call.go
@@ -2,7 +2,47 @@ package reflect
 
 import "reflect"
 
-func IterateFunc(entity any) map[string]FuncInfo {
+func IterateFunc(entity any) (map[string]FuncInfo, error) {
+	typ := reflect.TypeOf(entity)
+	//for typ.Kind() == reflect.Pointer {
+	//	typ = typ.Elem()
+	//}
+	numMethod := typ.NumMethod()
+	res := make(map[string]FuncInfo, numMethod)
+	for i := 0; i < numMethod; i++ {
+		method := typ.Method(i)
+		fn := method.Func
+		numIn := fn.Type().NumIn()
+		numOut := fn.Type().NumOut()
+
+		input := make([]reflect.Type, 0, numIn)
+		inputValue := make([]reflect.Value, 0, numIn)
+		output := make([]reflect.Type, 0, numOut)
+
+		inputValue = append(inputValue, reflect.ValueOf(entity))
+		input = append(input, reflect.TypeOf(entity))
+
+		for j := 1; j < numIn; j++ {
+			fnInType := fn.Type().In(j)
+			input = append(input, fnInType)
+			inputValue = append(inputValue, reflect.Zero(fnInType))
+		}
+		for j := 0; j < numOut; j++ {
+			output = append(output, fn.Type().Out(j))
+		}
+		resValues := fn.Call(inputValue)
+		result := make([]any, 0, len(resValues))
+		for _, v := range resValues {
+			result = append(result, v.Interface())
+		}
+		res[method.Name] = FuncInfo{
+			Name:        method.Name,
+			InputTypes:  input,
+			OutputTypes: output,
+			Result:      result,
+		}
+	}
+	return res, nil
 
 }
 
--- orm/reflect/func_call_test.go
+++ orm/reflect/func_call_test.go
@@ -2,6 +2,8 @@ package reflect
 
 import (
 	"github.com/stretchr/testify/assert"
+	"go-train/orm/reflect/types"
+	"reflect"
 	"testing"
 )
 
@@ -13,12 +15,51 @@ func TestIterateFunc(t *testing.T) {
 		wantRes map[string]FuncInfo
 		wantErr error
 	}{
-		{},
+		{
+			name:   "struct",
+			entity: types.NewUser("justfong", 18),
+			wantRes: map[string]FuncInfo{
+				"GetAge": {
+					Name:        "GetAge",
+					InputTypes:  []reflect.Type{reflect.TypeOf(types.User{})},
+					OutputTypes: []reflect.Type{reflect.TypeOf(0)},
+					Result:      []any{18},
+				},
+				//"ChangeName": {
+				//	Name:       "ChangeName",
+				//	InputTypes: []reflect.Type{reflect.TypeOf("")},
+				//OutputTypes: []reflect.Type{reflect.TypeOf(0)},
+				//Result:      []any{18},
+				//},
+			},
+		},
+		{
+			name:   "pointer",
+			entity: types.NewUserPtr("justfong", 18),
+			wantRes: map[string]FuncInfo{
+				"GetAge": {
+					Name:        "GetAge",
+					InputTypes:  []reflect.Type{reflect.TypeOf(&types.User{})},
+					OutputTypes: []reflect.Type{reflect.TypeOf(0)},
+					Result:      []any{18},
+				},
+				"ChangeName": {
+					Name:        "ChangeName",
+					InputTypes:  []reflect.Type{reflect.TypeOf(&types.User{}), reflect.TypeOf("")},
+					OutputTypes: []reflect.Type{},
+					Result:      []any{},
+				},
+			},
+		},
 	}
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
 			res, err := IterateFunc(tt.entity)
-			assert.Equal(t, tt.want, res)
+			assert.Equal(t, tt.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tt.wantRes, res)
 		})
 	}
 }
